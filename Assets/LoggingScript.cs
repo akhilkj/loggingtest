431e2a61c2505bce94cc2d3ae18eac5f210b95f1
Akhil K J <akhilkj@valorem.com>
2018-07-05 15:06:10 +0530

    log target changes

--- Assets/LoggingScript.cs
+++ Assets/LoggingScript.cs
@@ -1,34 +1,43 @@
-﻿using System;
-using NLog;
+﻿using NLog;
 using NLog.Config;
 using NLog.Targets;
 using UnityEngine;
 
-public class LoggingScript : MonoBehaviour {
+public class LoggingScript : MonoBehaviour
+{
     public static readonly NLog.LoggerSet _log = LoggerFactory.GetLogger(typeof(LoggingScript).Name);
     public UnityEngine.UI.InputField num1;
     public UnityEngine.UI.InputField num2;
-    Calculate obj = new Calculate();
+    private Calculate obj = new Calculate();
+    public NLog.Logger logger = LogManager.GetCurrentClassLogger();
+
+
     private void Start()
     {
-        obj.Logged += LogEntry;
     }
+
     public void Divide()
     {
-       
-        int a = int.Parse(num1.text);
-        int b = int.Parse(num2.text);
-        int x = obj.Div(a, b);
-        var config = new LoggingConfiguration();
         var fileTarget = new FileTarget("targetFile")
         {
             FileName = "C:/Users/Akhilkj/Documents/CalculatorTest/Logs/unityTest.log",
-            Layout = "${longdate} ${level} ${message}  ${exception}"
+            Layout = "${longdate}\t${logger}  [${level}]  ${message}  ${exception}"
         };
+
+
+        //logs given to NLog logger in unity and dlls will be saced in unityTest.log file
+        //logs given to _log is send to Papertrail
+        //DEbug.log logs to Console and Papertrail 
+
+
+        int a = int.Parse(num1.text);
+        int b = int.Parse(num2.text);
+        var config = new LoggingConfiguration();
         config.AddTarget(fileTarget);
         config.AddRule(LogLevel.Debug, LogLevel.Fatal, fileTarget, "*");
         LogManager.Configuration = config;
-        NLog.Logger logger = LogManager.GetCurrentClassLogger();
+        int x = obj.Div(a, b);
+
         //custom nlog made to store into file
         logger.Debug("Testing NLog");
 
@@ -37,30 +46,9 @@ public class LoggingScript : MonoBehaviour {
 
         //Unity log goes to unity console as well as Papertrail
         Debug.Log("xxxxxxxx");
-        
     }
 
-    private void LogEntry(string foo)
-    {
-        var config = new LoggingConfiguration();
-       var fileTarget = new FileTarget("targetFile")
-        {
-            FileName = "C:/Users/Akhilkj/Documents/CalculatorTest/Logs/dllLog.log",
-            Layout = "${longdate} ${level} ${message}  ${exception}"
-        };
-        config.AddTarget(fileTarget);
-        config.AddRule(LogLevel.Trace, LogLevel.Fatal, fileTarget, "*");
-        LogManager.Configuration = config;
-        NLog.Logger dllLog = LogManager.GetCurrentClassLogger();
-        //Log goes to custom file
-        dllLog.Debug(foo);
-        //Logs to PaperTrail
-        _log.Debug(foo);
-        //Log goes to Unity console as well as Papertrail
-        Debug.Log("unitydlllog: " + foo);
-    }
     private void OnDestroy()
     {
-        obj.Logged -= LogEntry;
     }
 }
